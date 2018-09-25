d7675184febe9c17e18dd930a8ae9d64d075dc2c
Atil Samancioglu <samancioglu.atil@gmail.com>
2018-09-25 13:34:20 +0300

    Initial Commit

---
+++ Parse Insta Clone/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Parse Insta Clone
+//
+//  Created by Atil Samancioglu on 25.09.2018.
+//  Copyright © 2018 Atil Samancioglu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Parse Insta Clone/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Parse Insta Clone
+//
+//  Created by Atil Samancioglu on 25.09.2018.
+//  Copyright © 2018 Atil Samancioglu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
